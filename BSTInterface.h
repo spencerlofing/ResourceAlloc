ba195d4506f16eea564ee3392bfb9b7559d1207f
spencerlofing <spencerlofing@gmail.com>
2016-05-15 17:38:12 -0600

    Update and rename BST.h to BSTInterface.h

---
+++ BSTInterface.h
@@ -0,0 +1,12 @@
+#ifndef BSTINTERFACE_H
+#define BSTINTERFACE_H
+#include "Node.h"
+class BST
+{
+	private:
+		Node<T>* m_root;
+	public:
+		virtual ~BSTInterface();
+		virtual void insert(Node<T>* newNode)=0;
+};
+#endif
