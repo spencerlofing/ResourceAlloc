63567ffab8c204c60e0f3113474371221a90e155
spencerlofing <spencerlofing@gmail.com>
2016-05-15 17:28:30 -0600

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -5,3 +5,23 @@ Node<T>::Node
 	m_left = nullptr;
 	m_right = nullptr;
 }
+template <typename T>
+Node<T>* Node<T>::getLeft()
+{
+	return m_left;	
+}
+template <typename T>
+Node<T>* Node<T>::getRight()
+{
+	return m_right;
+}
+template <typename T>
+void Node<T>::setLeft(Node<T> left)
+{
+	m_left = left;
+}
+template <typename T>
+void Node<T>::setRight(Node<T> right)
+{
+	m_right = right;
+}
