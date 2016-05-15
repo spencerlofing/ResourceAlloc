851208f1b4d7884290967636247f96c2fd550c03
spencerlofing <spencerlofing@gmail.com>
2016-05-15 17:30:22 -0600

    Update Node.h

--- Node.h
+++ Node.h
@@ -3,8 +3,15 @@
 template <typename T>
 class Node
 {
+private:
 	Node<T>* m_left;
 	Node<T>* m_right;
+public:
+	Node();
+	Node<T>* getLeft();
+	Node<T>* getRight();
+	void setLeft(Node<T>* left);
+	void setRight(Node<T>* right);
 };
 
 #endif //NODE_H
